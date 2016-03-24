883a0fc4c930f2fa72f6621c9da068566d42c854
Eric Fontaine <eric@ericfontainejazz.com>
2016-03-24 11:45:39 -0400

    try unmounting binformat misc first...

--- build/travis/job2_AppImage/build.sh
+++ build/travis/job2_AppImage/build.sh
@@ -54,6 +54,7 @@ fi
 #esac
 
 # associate arm binaries with qemu-arm-static.  https://resin.io/blog/building-arm-containers-on-any-x86-machine-even-dockerhub/
+sudo umount binfmt_misc
 sudo mount binfmt_misc -t binfmt_misc /proc/sys/fs/binfmt_misc
 echo ':arm:M::\x7fELF\x01\x01\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x02\x00\x28\x00:\xff\xff\xff\xff\xff\xff\xff\x00\xff\xff\xff\xff\xff\xff\xff\xff\xfe\xff\xff\xff:/usr/bin/qemu-arm-static:' > sudo /proc/sys/fs/binfmt_misc/register  
 
