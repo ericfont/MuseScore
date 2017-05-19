af724bdcef5c64cb06de15088fc3beeff9b5a45e
Eric Fontaine <eric@ericfontainejazz.com>
2017-05-18 22:57:02 -0400

    fix #202751 SegFault delete SFont while loadSamples()
    Mutex may be locked if another thread is trying to load, add, or delete soundfonts.
    
    If another thread deletes a soundfont, trying to read the zone info will result in a SEGFAULT as described in issue #202751.
    
    These operations need to be mutually-exclusive, but since Seq::process() is a realtime context, we can't block here.
    
    In order to keep Seq::process() running, my solution here is to simply not execute the rest of loadSamples() if another thread had locked that mutex.

--- fluid/sfont.cpp
+++ fluid/sfont.cpp
@@ -137,7 +137,9 @@ Preset::~Preset()
 
 void Preset::loadSamples()
       {
-//      sfont->synth->mutex.lock();
+      if (!sfont->synth->mutex.tryLock())
+            return;
+
       if (_global_zone && _global_zone->instrument) {
             Instrument* i = _global_zone->instrument;
             if (i->global_zone && i->global_zone->sample)
@@ -153,7 +155,8 @@ void Preset::loadSamples()
             foreach(Zone* iz, i->zones)
                   iz->sample->load();
             }
-//      sfont->synth->mutex.unlock();
+
+      sfont->synth->mutex.unlock();
       }
 
 //---------------------------------------------------------
